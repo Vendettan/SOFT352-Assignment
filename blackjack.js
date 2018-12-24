edbd09e6766e76cb6871691bc5efdca84e5a816b
Matt Bell <matthew.d.bell@students.plymouth.ac.uk>
2018-12-24 11:02:18 +0000

    Implemented printing of cards to a canvas
    Working out how to access file names of the cards to create the deck

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,32 @@
+function Card(value, suit)
+{
+  this.value = value;
+  this.suit = suit;
+  imageName = (value + "_of_" + suit);
+  var img = document.createElement('img');
+  img.src = "CardImages/" + imageName + ".png"
+  this.image = img;
+}
+
+function getImage()
+{
+  var aceofspades = new Card("ace", "spades");
+  var canvas = $("#dealerCanvas");
+  var context = canvas[0].getContext("2d");
+  context.drawImage(aceofspades.image,0,0,50,80);
+
+  getDeck();
+}
+
+function getDeck()
+{
+  var deck = [];
+  path = "CardImages/"
+
+  var fs = require('fs');
+  var files = fs.readdirSync('CardImages');
+}
+
 function Hit()
 {
 
@@ -13,7 +42,12 @@ function DoubleDown()
 
 }
 
+function Split()
+{
+
+}
+
 function Bet()
 {
-  
+
 }
