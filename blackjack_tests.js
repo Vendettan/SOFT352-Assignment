53bc35d66c337895040d25b363476f46ba589f09
Matt Bell <32261725+Vendettan@users.noreply.github.com>
2019-01-08 15:25:58 +0000

    Update blackjack_tests.js

--- blackjack_tests.js
+++ blackjack_tests.js
@@ -22,3 +22,39 @@ QUnit.test("Checking player total function returns correctly", function(assert)
   assert.equal(player2.hand.length, 2, "Check player hand length is 2");
   assert.equal(player2.Total(), 14, "Check player Total() function returns correctly");
 });
+
+QUnit.test("Checking player total function returns correctly with an Ace", function(assert)
+{
+  var player3 = new Player("socket", "player3", "David");
+
+  assert.ok(player3, "Check player is not null");
+
+  var ace = new Card("ace_of_spades.png");
+  var four = new Card("4_of_diamonds.png");
+
+  player3.hand.push(king, four);
+
+  assert.equal(player3.Total(), 15, "Check player Total() function returns correctly with ace as 11");
+
+  var nine = new Card("9_of_hearts.png");
+
+  player3.hand.push(seven);
+
+  assert.equal(player3.Total(), 14, "Check player Total() function returns correctly with ace as 1");
+});
+
+QUnit.test("Testing dealer hits when hand total is under 17", function()
+{
+  var dealer = new Dealer();
+
+  assert.ok(dealer, "Check dealer is not null");
+
+  var king = new Card("king_of_hearts.png");
+  var four = new Card("4_of_diamonds.png");
+
+  dealer.hand.push(king, four);
+
+  DealersTurn();
+
+  assertTrue(dealer.hand.length > 2, "Checking dealer hand has more than 2 cards (i.e. has hit)");
+});
